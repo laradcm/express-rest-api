4f00404f987ecfbac998a8bc694def861b053826
Lara Mauricio <laradcm@gmail.com>
2023-07-03 18:27:00 -0400

    finished user CRUD operations

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { get, merge } from "lodash";
+import { getUserBySessionToken } from "../db/users";
 
 export function reqTracking( req: express.Request, res: express.Response, next: express.NextFunction )
 {
@@ -6,3 +8,51 @@ export function reqTracking( req: express.Request, res: express.Response, next: 
     next();
 }
 
+export const isAuthenticated = async ( req: express.Request, res: express.Response, next: express.NextFunction ) =>
+{
+
+    try {
+
+        const sessionToken = req.cookies[ 'LARA-AUTH' ];
+
+        if ( !sessionToken ) {
+            return res.sendStatus( 403 );
+        }
+
+        const existingUser = await getUserBySessionToken( sessionToken );
+
+        if ( !existingUser ) {
+            return res.sendStatus( 403 );
+        }
+
+        merge( req, { identity: existingUser } );
+
+        next();
+
+    } catch ( error ) {
+
+        console.log( error );
+        return res.sendStatus( 400 );
+    }
+
+};
+
+export const isOwner = async ( req: express.Request, res: express.Response, next: express.NextFunction ) =>
+{
+    try {
+        const { id } = req.params;
+        const currentUserId = get( req, "identity._id" ) as string;
+
+        if ( !currentUserId ||
+             currentUserId.toString() !== id ) {
+
+            return res.sendStatus( 403 );
+        }
+
+
+        next();
+    } catch ( error ) {
+        console.log( error );
+        return res.sendStatus( 400 );
+    }
+};
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import authentication from "./authentication";
+import users from "./users";
 
 const router = express.Router();
 
 export default (): express.Router => //when you use the default keyword, on the import it is no necessary to use { }
 {
     authentication( router );
+    users( router );
     return router;
 }
 
